dc333496a646dec8e0606e112d12a1eac25ecd0c
Michal Kováčik <xkovacikm2@gmail.com>
2026-07-10 13:45:15 +0200

    remove X-Frame-Options header from default response headers (#53)
    Co-authored-by: Michal Kováčik <michal.kovacik@siemens-healthineers.com>

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,8 @@ module SsdVirtualBattery
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+    config.action_dispatch.default_headers.delete("X-Frame-Options")
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
